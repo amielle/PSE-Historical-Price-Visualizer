f51279d281a13f97c8590d7b5c763e8b6a59dc57
amielle <amielle.dulay@eee.upd.edu.ph>
2019-09-12 21:18:01 +0800

    Updated directories for proper referencing of files

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(__dirname + '/src'));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/src/index.html'));
 });
+
 
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
